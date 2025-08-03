6eb1eb684df2736b4f6d7800ddf3fcf07026644f
Mufafa98 <marian.parlea2@gmail.com>
2025-08-03 20:37:19 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <BrowserRouter basename="/Chromatica">
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
-
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+  </StrictMode>,
+)
